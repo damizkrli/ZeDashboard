03de0309aa42b968e86bb03aed4b6ebb69e4d5dd
damizkrli <karli.damien@gmail.com>
2022-11-08 21:29:25 +0100

    sort results

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends AbstractController
         $acceptedApplies = $calculateRules->calculateAcceptedApplies();
 
         return $this->render('home/index.html.twig', [
-            'apply_for' => $this->applyForRepository->findBy([], ['dateApplyFor' => 'DESC']),
+            'apply_for' => $this->applyForRepository->findBy([], ['jobTitle' => 'ASC']),
             'totalApplies' => $totalApplies,
             'sentApplies' => $sentApplies,
             'noResponseApplies' => $noResponseApplies,
